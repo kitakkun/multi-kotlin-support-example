78f6ca0269806ac0f455f356ed903a245c3a63d6
kitakkun <48154936+kitakkun@users.noreply.github.com>
2024-12-14 17:22:34 +0900

    ステップ0: プロジェクトのセットアップ

---
+++ settings.gradle.kts
@@ -0,0 +1,15 @@
+rootProject.name = "multi-kotlin-support-example"
+
+pluginManagement {
+    repositories {
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
+
+include(":multi-kotlin-module")
